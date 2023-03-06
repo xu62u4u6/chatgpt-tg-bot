bd125751520c0c8d2d094569fb2fbc6ef3117eea
blueorange <199928ltyos@gmail.com>
2023-03-07 02:10:06 +0800

    add send keyboard method

--- tg_bot.py
+++ tg_bot.py
@@ -1,6 +1,6 @@
 import requests
 import configparser
-import time 
+import time
 import openai
 
 config = configparser.ConfigParser()
@@ -20,57 +20,74 @@ class Message:
         pass
 """
 
+
 class TG_Bot:
     def __init__(self):
         openai.api_key = config["openai"]["key"]
         self.token = config["telegram"]["token"]
         self.webhook_url = config["telegram"]["webhook-url"]
+        self.send_message_url = f"https://api.telegram.org/bot{self.token}/sendMessage"
         self.init_time = time.time()
-        # chat_id: [msg]
         self.users_msgs = {}
-        
+
     def send_message(self, chat_id, text):
-        url = f'https://api.telegram.org/bot{self.token}/sendMessage'
         payload = {'chat_id': chat_id, 'text': text}
-        return requests.post(url,json=payload)
-    
+        return requests.post(self.send_message_url, json=payload)
+
+    def send_inline_keyboard(self, chat_id, text):
+        payload = {'chat_id': chat_id,
+                   'text': text,
+                   'reply_markup': {"inline_keyboard": [[{"text": "Button 1", "callback_data": "data1"}],
+                                                        [{"text": "Button 2", "callback_data": "data2"}]]}}
+        return requests.post(self.send_message_url, json=payload)
+
+    def send_keyboard(self, chat_id, text):
+        payload = {'chat_id': chat_id,
+                   'text': text,
+                   'replyMarkup': {"keyboard": [["Option 1", "Option 2"],
+                                                ["Option 3", "Option 4"]],
+                                   "one_time_keyboard": True}}
+        return requests.post(self.send_message_url, json=payload)
+
     def parse_message(self, msg):
         msg_dir = {}
         msg_dir["date"] = msg["date"]
-        msg_dir["chat_id"]  = msg["chat"]["id"]
+        msg_dir["chat_id"] = msg["chat"]["id"]
         msg_dir["text"] = msg["text"]
         for i in ["username", "first_name", "last_name"]:
             if i in msg.keys():
                 msg_dir[i] = msg[i]
             else:
                 msg_dir[i] = None
-        
+
         return msg_dir
-    
+
     def set_webhook(self):
         url = f'https://api.telegram.org/bot{self.token}/setWebhook?url={self.webhook_url}'
         res = requests.post(url)
         return res.status_code, res.text
-    
 
     def reset(self, chat_id, role="assistant"):
         self.users_msgs[chat_id] = [
             {"role": "system", "content": f"You are a {role}."}
         ]
 
+    def set_role(self, chat_id, role):
+        self.users_msgs[chat_id][0]["content"] = f"You are a {role}."
+
     def completion(self, chat_id, text):
         # if new
         if chat_id not in self.users_msgs.keys():
             self.reset(chat_id)
-        
+
         self.users_msgs[chat_id].append(
             {"role": "user", "content": text}
         )
-        
-        res = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=self.users_msgs[chat_id])
+
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo", messages=self.users_msgs[chat_id])
         reply = res.choices[0].message.content
         self.users_msgs[chat_id].append(
             {"role": "assistant", "content": reply}
         )
         return reply
-        
